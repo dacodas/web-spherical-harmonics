bba26134306df1ef796e82c532d6aface0614386
Dacoda Strack <dacoda@fedora.omen.home>
2020-05-05 11:01:27 -0700

    old changes to wavelet.py

--- src/animation-testing/wavelet.py
+++ src/animation-testing/wavelet.py
@@ -3,8 +3,12 @@ import numpy as np
 from scipy.stats import norm
 from scipy.signal import morlet
 
-wavelet = np.real(morlet(100))
-wavelet = np.interp(wavelet, (wavelet.min(), wavelet.max()), (0, 255))
-int_array = ( np.repeat([wavelet], 100, 0) ).astype(np.uint8) 
+image_size = 1000
+max_gray = 65535
+dtype = np.uint16
+
+wavelet = np.real(morlet(image_size))
+wavelet = np.interp(wavelet, (wavelet.min(), wavelet.max()), (0, max_gray))
+int_array = ( np.repeat([wavelet], image_size, 0) ).astype(dtype)
 
 Image.fromarray(int_array).save("build/wavelet.png")
