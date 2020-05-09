7a22859753d360b6e3f1d554d6c3f841a83e2a60
Dacoda Strack <dacoda@fedora.omen.home>
2020-05-08 19:05:01 -0700

    remove normals texture, add gridlines texture, draw gridlines from texture

--- src/shaders/surface/fragment.glsl
+++ src/shaders/surface/fragment.glsl
@@ -6,7 +6,8 @@ varying vec3 position_tpr;
 varying vec3 normal;
 
 uniform sampler2D rho_sampler;
-uniform sampler2D normals_sampler;
+// uniform sampler2D normals_sampler;
+uniform sampler2D gridlines_sampler;
 uniform float max_amplitude;
 uniform float amplitude;
 uniform vec3 light_position;
@@ -49,14 +50,14 @@ void main()
 	const float specularStrength = 0.2;
 	const vec3 ambient = ambientStrength * lightColor;
 
-	vec3 objectColor = vec3(0.5 + 0.2 * sin(position_tpr.y), 0, 0);
+	vec3 objectColor = vec3(0.5 + 0.2 * sin(position_tpr.y), 0, 0.5 + 0.2 * cos(position_tpr.x));
 	vec3 color;
 
 	if ( normal_coloring )
 	{
 		color = colorNormal(normal);
 	}
-	else 
+	else
 	{
 		vec3 light_direction = normalize(light_position - position);
 		vec3 view_direction = normalize(view_position - position);
@@ -71,30 +72,39 @@ void main()
 		color = ( ambient + diffuse + specular ) * objectColor;
 	}
 
-	if ( draw_gridlines ) { color = gridline(color); }
+	vec4 color_a;
+	if ( draw_gridlines )
+	{
+		vec4 grid = texture2D( gridlines_sampler, texture_coordinates );
+		color_a = mix(vec4(color, 1.0), grid, grid.a);
+	}
+	else
+	{
+		color_a = vec4(color, 1.0);
+	}
 
-	gl_FragColor = vec4(color , 1.0 );
+	gl_FragColor = color_a;
 }
 
-vec3 oldNormal() 
-{
-	// Why does the lighting only line up when subtracting 45 degrees from phi
-	vec3 normal_tpr = ( 2.0 * texture2D( normals_sampler, texture_coordinates ) - 1.0 ).xyz;
-
-	float rho = position_tpr.z;
-	float theta = position_tpr.x;
-	float phi = position_tpr.y;
-
-	// We convert unit vectors here, not components
-	// http://www.physics.purdue.edu/~jones105/phys310/coordinates.pdf
-	vec3 normal_xyz = mat3(
-		rho * cos(theta) * cos(phi),   rho * cos(theta) * sin(phi),  rho * sin(theta),
-		-rho * sin(theta) * sin(phi),  rho * sin(theta) * cos(phi),   0,
-		sin(theta) * cos(phi),         sin(theta) * sin(phi),         cos(theta)
-	) * normal_tpr;
-
-	vec3 norm = - normalize(normal_xyz);
-
-	return norm;
-}
-
+// vec3 oldNormal()
+// {
+// 	// Why does the lighting only line up when subtracting 45 degrees from phi
+// 	vec3 normal_tpr = ( 2.0 * texture2D( normals_sampler, texture_coordinates ) - 1.0 ).xyz;
+//
+// 	float rho = position_tpr.z;
+// 	float theta = position_tpr.x;
+// 	float phi = position_tpr.y;
+//
+// 	// We convert unit vectors here, not components
+// 	// http://www.physics.purdue.edu/~jones105/phys310/coordinates.pdf
+// 	vec3 normal_xyz = mat3(
+// 		rho * cos(theta) * cos(phi),   rho * cos(theta) * sin(phi),  rho * sin(theta),
+// 		-rho * sin(theta) * sin(phi),  rho * sin(theta) * cos(phi),   0,
+// 		sin(theta) * cos(phi),         sin(theta) * sin(phi),         cos(theta)
+// 	) * normal_tpr;
+//
+// 	vec3 norm = - normalize(normal_xyz);
+//
+// 	return norm;
+// }
+//
